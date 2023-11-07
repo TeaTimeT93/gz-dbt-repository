0cbf96e48f4136b3e576da7b4a60757a9b229373
tung.ttn9@gmail.com <tung.ttn9@gmail.com>
2023-11-07 14:52:06 +0000

    gl

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,23 +1,11 @@
-WITH tabl1 AS(
-    select date_date,
-           orders_id,
-        products_id,
-revenue,
-quantity,
-CAST(purchase_price AS FLOAT64) AS purchase_price,
-ROUND(revenue-quantity*CAST(purchase_price AS FLOAT64),2) AS margin
-FROM {{ref("stg_raw__sales")}} 
-LEFT JOIN {{ref("stg_raw__product")}}
-USING (products_id) 
-)
-
 SELECT orders_id,date_date,
-    (revenue) AS revenue,
-	ROUND((quantity),2) as quantity,
-	ROUND((purchase_price),2) as purchase_price,
-	ROUND((margin),2) as margin,
-    ROUND((margin+s.shipping_fee-s.logcost-CAST(s.ship_cost AS FLOAT64),2)) AS op_margin
-FROM tabl1
+    SUM(revenue) AS revenue,
+	ROUND(SUM(quantity),2) as quantity,
+	ROUND(SUM(purchase_price),2) as purchase_price,
+	ROUND(SUM(margin),2) as margin,
+    ROUND(SUM((margin+s.shipping_fee-s.logcost-CAST(s.ship_cost AS FLOAT64))),2) AS op_margin
+FROM {{ref("int_sales_margin")}}
 LEFT JOIN {{ref("stg_raw__ship")}} s
 USING (orders_id)
+GROUP BY orders_id,date_date
 ORDER BY orders_id DESC
---
+++ models/staging/finance_days.sql
@@ -0,0 +1,27 @@
+WITH tab1 AS (
+SELECT
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.logcost + CAST(s.ship_cost AS FLOAT64)),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_price
+  ,o.margin
+  ,s.shipping_fee
+  ,s.logcost
+  ,s.ship_cost
+FROM `modular-analog-402210`.`dbt_`.`int_orders_operational` o
+LEFT JOIN `modular-analog-402210`.`dbt_`.`stg_raw__ship` s 
+	USING(orders_id)
+ORDER BY orders_id desc
+)
+
+SELECT date_date,
+       COUNT(orders_id) AS nb_transaction,
+       ROUND(SUM(revenue),0) AS revenue,
+       ROUND(AVG(revenue),1) AS average_basket,
+       ROUND(SUM(margin),0) AS margin,
+       ROUND(SUM(op_margin),0) AS operational_margin 
+FROM {{ref("int_orders_operational")}}
+GROUP BY date_date
+ORDER BY date_date DESC
